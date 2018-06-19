917bcef8b2fb46d848c6eec55157aaf1cbf65a77
shlomys <sellashlomy@gmail.com>
2018-06-19 10:10:51 +0300

    set up express

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req,res)=> {
+  res.send({
+    message:'hello world'
+  })
+})
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  res.send(req.params)
+})
+app.listen(process.env.PORT || 8081 ,()=> console.log('ssssss'))
